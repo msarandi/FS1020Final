2715fcf861c832ace1443aeaf5f2f426b3e745f2
msarandi <mariesarandi@gmail.com>
2019-11-30 14:55:43 -0500

    Added 'GET' command to test

--- src/index.js
+++ src/index.js
@@ -4,3 +4,11 @@ let express = require('express');
 
 let app = express();
 let port = 3000;
+
+app.listen(port, function () {
+  console.log(`Express server listening on port ${port}.`);
+});
+
+app.get('/', function(request, response) {
+  response.send('Hello, World!');
+});
