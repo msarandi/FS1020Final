ed66682415b5e971b56c1b35fa9295b22f3ec78d
msarandi <mariesarandi@gmail.com>
2019-12-14 11:20:01 -0500

    DB add user

--- src/router.js
+++ src/router.js
@@ -11,16 +11,27 @@ let router = express.Router();
 
 // Routes go under here
 
+function validateUserMiddleware (request, response, next) {
+  let user = req.body;
+  if (!user.name) {
+    res.status(400).send('"Username" is a required field');
+  } else if (!user.pass) {
+    res.status(400).send('"Password" is a required field');
+  } else {
+    next();
+  }
+}
+
+router.post('/register/user/new', async function (request, response) {
 
 
+// Add user to DB and send status code
 
-// Add car to DB and send status code
+db.addUser(req.body).then(function() {
+  res.sendStatus(201);
+});
+});
 
-// Alternative using `promise.then`
-// router.post('/car', async function (req, res) {
-//   db.addCar(req.body).then(function () {
-//     res.sendStatus(201);
-//   });
-// });
+
 
 module.exports = router;
